eb596b025ef08a56603a1f6e6bf037d988a4d1e8
tivler <thomasdmckyer@gmail.com>
2019-05-05 01:27:44 -0400

    stuff

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,28 +1,24 @@
-// let navStuff = () => {
-//     let offset = window.pageYOffset;
-//     if (offset >= )
-// }
-
-let offset = window.pageYOffset;
-console.log(offset);
-
-function parallax () {
-    const parallax = document.querySelector('.parallax');
-
-    window.addEventListener('scroll' ,  function () {
-        let offset = window.pageYOffset;
-        parallax.style.backgroundPositionY = offset * 0.4 + 'px' ; 
-    });
-}
-
-function animateCss (item , action) {
+function animateCss (item , action , delay) {
     const element =  document.querySelector(item);
-    element.classList.add('animated', action);
+    element.classList.add('animated', action , delay);
 }
 
-parallax();
-
-animateCss(".main_nav" , "bounceInDown");
+animateCss(".main_nav" , "bounceInDown" );
 animateCss(".main_header--title", "bounceInUp");
+animateCss('.header--description' , "bounceInUp");
+animateCss('.button' , "bounceInUp");
 
 
+function navEffect () {
+    const nav = document.querySelector('.main_nav');
+
+    window.addEventListener('scroll' , function() {
+        if(window.scrollY) {
+            nav.classList.add('blue');
+        } else {
+            nav.classList.remove('blue');
+        }
+    })
+}
+
+navEffect();
