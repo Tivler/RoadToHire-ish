164b80c1de404ce290fb194115fd3ab8238a2b41
tivler <thomasdmckyer@gmail.com>
2019-05-14 12:54:38 -0400

    moved post photos into a folder

---
+++ food/food.js
@@ -0,0 +1,8 @@
+function animateCss(item, action) {
+    const element = document.querySelector(item);
+    element.classList.add('animated', action);
+}
+
+animateCss(".main_nav", "bounceInDown");
+animateCss(".header_title", "bounceInUp");
+animateCss('.header_description', "bounceInUp");
