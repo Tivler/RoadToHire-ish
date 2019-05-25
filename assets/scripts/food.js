d6df502c9b8e06242f10a52fa44758ceb786f85a
tivler <thomasdmckyer@gmail.com>
2019-05-25 10:48:33 -0400

    deleted food page

--- assets/scripts/food.js
+++
@@ -1,8 +0,0 @@
-function animateCss(item, action) {
-    const element = document.querySelector(item);
-    element.classList.add('animated', action);
-}
-
-animateCss(".main_nav", "bounceInDown");
-animateCss(".header_title", "bounceInUp");
-animateCss('.header_description', "bounceInUp");
